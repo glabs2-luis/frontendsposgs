19203cef4fed3bf66984a6d6f58918c8abeaf77c
Juan Pablo Laptop Work <shunikv@gmail.com>
2025-08-07 12:35:18 -0600

    mejorar la eliminacion de productos, manejo de error en facturacion, y meojorar la actualizacion de la tabla de productos add to pedidos

--- src/modules/pos/components/Mainpos.vue
+++ src/modules/pos/components/Mainpos.vue
@@ -1,24 +1,19 @@
 <template>
   <q-page class="q-pa-md">
-     
     <!-- Cliente Formulario -->
     <ClienteForm />
-    
+
     <!-- <q-img src="/img/2.png" alt="Logo del sistema"
         style="max-width: 200px; border-radius: 8px;"
         spinner-color="yellow-9"
           class="absolute-top-right q-ma-md"/> -->
 
-    
-
-     <!-- Tabla de productos -->
     <!-- Tabla de productos -->
-
-
-</q-page>
+    <!-- Tabla de productos -->
+  </q-page>
 </template>
 
 <script setup lang="ts">
-import ClienteForm from '../pages/clienteForm.vue'
-import ProductosTab from '../pages/productosTab.vue'
+import ClienteForm from "../pages/clienteform.vue";
+import ProductosTab from "../pages/productostab.vue";
 </script>
