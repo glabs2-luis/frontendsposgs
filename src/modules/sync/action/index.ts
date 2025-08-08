a1562ab8d3bd82b08f23420f2fa39dc0cb594a31
Juan Pablo Laptop Work <shunikv@gmail.com>
2025-08-08 15:07:23 -0600

    sinconizando data de server local con el intermedio, mod 98%

---
+++ src/modules/sync/action/index.ts
@@ -0,0 +1,6 @@
+export * from "./update-codigosBarra.action";
+export * from "./update-cuponesEnc.action";
+export * from "./update-cuponesDet.action";
+export * from "./update-productos.action";
+export * from "./update-precios.action";
+export * from "./update-vendedores.action";
---
+++ src/modules/sync/action/update-vendedores.action.ts
@@ -0,0 +1,16 @@
+import { getAxiosErrorMessage } from "@/common/helper/geterrordb";
+import posApi from "@/api/apiPos";
+
+export const updateVendedoresAction = async (): Promise<void> => {
+  try {
+    const { data } = await posApi.post(`/sync/update-vendedores`);
+    return data;
+  } catch (error) {
+    console.log(error);
+    const message = getAxiosErrorMessage(
+      error,
+      "Hubo un error al momento de sincronizar los vendedores al sistema local de ventas"
+    );
+    throw new Error(message);
+  }
+};
