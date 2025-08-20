a9f59b01bbe0776d0496d8684ada69f885abb462
Luis Alvarado <desarrollo2.glabs@outlook.com>
2025-08-20 16:10:16 -0600

    Diseño factura terminada parte 2 xd

--- src/modules/pos/pages/productostab.vue
+++ src/modules/pos/pages/productostab.vue
@@ -1655,7 +1655,7 @@ const imprimirFactura = async (data) => {
     subtotal: item.SUBTOTAL_GENERAL.toFixed(4),
   }));
 
-  console.log("detalle: ", detalle);
+  console.log('detalle: ', detalle)
 
   const totalItems = itemsFactura.reduce(
     (total, item) => total + Number(item.cantidad),
