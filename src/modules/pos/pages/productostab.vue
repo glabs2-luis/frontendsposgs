8b41ee595a386be82b9c265124fe4ac7e7d13178
Luis Alvarado <desarrollo2.glabs@outlook.com>
2025-08-20 14:39:11 -0600

    Diseño factura Terminada

--- src/modules/pos/pages/productostab.vue
+++ src/modules/pos/pages/productostab.vue
@@ -1615,7 +1615,7 @@ const confirmarFactura = async () => {
       // Asignar este valor para llenar la factura
       idFacturaEnc.value = respuesta.ID_FACTURA_ENC;
       // Ahora sí espera a que termine la certificación
-
+      
       if (contingencia.value === true) {
         await imprimirFactura(respuesta);
         return;
@@ -1636,6 +1636,7 @@ const confirmarFactura = async () => {
 };
 
 const imprimirFactura = async (data) => {
+
   console.log("imprimiendo factura...");
   const factura2 = await obtenerFacturaId3(idFacturaEnc.value);
 
