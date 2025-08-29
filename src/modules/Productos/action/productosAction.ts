0c5b1619e62e9347296f4d485fa0d105af5c3c6d
Juan Pablo Laptop Work <shunikv@gmail.com>
2025-08-29 14:26:37 -0600

    cambios en productos action

--- src/modules/Productos/action/productosAction.ts
+++ src/modules/Productos/action/productosAction.ts
@@ -1,36 +1,55 @@
-import posApi from "@/api/apiPos"
-import { getAxiosErrorMessage } from "@/common/helper/geterrordb"
-import { Productos } from "../interfaces/productosInterface"
+import posApi from "@/api/apiPos";
+import { getAxiosErrorMessage } from "@/common/helper/geterrordb";
+import { Productos } from "../interfaces/productosInterface";
 
 // Obtener todos los productos
 export const obtenerProductosAction = async (): Promise<Productos[]> => {
-    try {
-        const { data } = await posApi.get<Productos[]>(`/maestro-productos`)
-        return data
-    } catch (error){
-        const message = getAxiosErrorMessage(error, "Hubo un error obteniendo todos los productos")
-        throw new Error(message)
-    }
-}
+  try {
+    const { data } = await posApi.get<Productos[]>(`/maestro-productos`);
+
+    return data;
+  } catch (error) {
+    const message = getAxiosErrorMessage(
+      error,
+      "Hubo un error obteniendo todos los productos"
+    );
+    throw new Error(message);
+  }
+};
 
 // por Id
-export const obtenerProductosIdAction = async (producto: string) : Promise<Productos[]> => {
-    try {
-        const { data } = await posApi.get<Productos[]>(`/maestro-productos/${producto}`)
-            return data
-        } catch (error) {
-            const message = getAxiosErrorMessage(error, "Hubo un error obteniendo los productos por Id")
-            throw new Error(message)
-        }
-    }
+export const obtenerProductosIdAction = async (
+  producto: string
+): Promise<Productos[]> => {
+  try {
+    const { data } = await posApi.get<Productos[]>(
+      `/maestro-productos/${producto}`
+    );
+    return data;
+  } catch (error) {
+    const message = getAxiosErrorMessage(
+      error,
+      "Hubo un error obteniendo los productos por Id"
+    );
+    throw new Error(message);
+  }
+};
 
-    // Obtener Precio
-export const ObtenerProductosPrecioAction = async (producto: string, cantidad: number): Promise<Productos> => {
-    try{
-        const { data } = await posApi.get<Productos>(`/maestro-productos/precioProducto/${producto}/${cantidad}`)
-        return data
-    } catch (error) {
-        const message = getAxiosErrorMessage(error, "Hubo un error al obtener el precio del producto")
-        throw new Error(message)
-    }
-}
+// Obtener Precio
+export const ObtenerProductosPrecioAction = async (
+  producto: string,
+  cantidad: number
+): Promise<Productos> => {
+  try {
+    const { data } = await posApi.get<Productos>(
+      `/maestro-productos/precioProducto/${producto}/${cantidad}`
+    );
+    return data;
+  } catch (error) {
+    const message = getAxiosErrorMessage(
+      error,
+      "Hubo un error al obtener el precio del producto"
+    );
+    throw new Error(message);
+  }
+};
